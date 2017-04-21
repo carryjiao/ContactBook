749a100dc467cea65cd5b5d8eb15f342b5e30521
carryjiao <465938001@qq.com>
2017-04-21 15:47:19 +0800

    add SwipeMenuListView and call Permission

--- app/src/main/java/com/carryj/root/contactbook/fragments/ContactBookFragement.java
+++ app/src/main/java/com/carryj/root/contactbook/fragments/ContactBookFragement.java
@@ -2,19 +2,25 @@ package com.carryj.root.contactbook.fragments;
 
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.carryj.root.contactbook.R;
 import com.carryj.root.contactbook.adapter.ContactBookAdapter;
 
@@ -30,7 +36,7 @@ public class ContactBookFragement extends Fragment implements OnClickListener {
     private TextView tv_contact_book_add;
     private ImageView iv_contact_book_box;
     private ContactBookAdapter adapter;
-    private ListView listView;
+    private SwipeMenuListView listView;
     private ArrayList<String> nameListData = new ArrayList<String>();
 
     /**获取库Phon表字段**/
@@ -42,6 +48,21 @@ public class ContactBookFragement extends Fragment implements OnClickListener {
 
 
     public ContactBookFragement() {
+        nameListData.add("焦消");
+        nameListData.add("王舒");
+        nameListData.add("周身高");
+        nameListData.add("焦消");
+        nameListData.add("王舒");
+        nameListData.add("周身高");
+        nameListData.add("焦消");
+        nameListData.add("王舒");
+        nameListData.add("周身高");
+        nameListData.add("焦消");
+        nameListData.add("王舒");
+        nameListData.add("周身高");
+        nameListData.add("焦消");
+        nameListData.add("王舒");
+        nameListData.add("周身高");
 
     }
 
@@ -49,9 +70,6 @@ public class ContactBookFragement extends Fragment implements OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contact_book,null);
         //getPhoneContacts();
-        nameListData.add(0,"焦消");
-        nameListData.add(1,"王舒");
-        nameListData.add(2,"周身高");
         initView(view);
         return view;
     }
@@ -59,9 +77,74 @@ public class ContactBookFragement extends Fragment implements OnClickListener {
     private void initView(View view) {
         tv_contact_book_add = (TextView) view.findViewById(R.id.tv_contact_book_add);
         iv_contact_book_box = (ImageView) view.findViewById(R.id.iv_contact_book_box);
-        listView = (ListView) view.findViewById(R.id.contact_book_listview);
+        listView = (SwipeMenuListView) view.findViewById(R.id.contact_book_listview);
         adapter = new ContactBookAdapter(getContext(), nameListData);
         listView.setAdapter(adapter);
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                /*// create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(getContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(dp2px(90));
+                // set item title
+                openItem.setTitle("Open");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);*/
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(dp2px(60));
+                /*// set a icon
+                deleteItem.setIcon(R.drawable.ic_delete);*/
+                // set item title
+                deleteItem.setTitle("删除");
+                // set item title fontsize
+                deleteItem.setTitleSize(18);
+                // set item title font color
+                deleteItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+// set creator
+        listView.setMenuCreator(creator);
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+
+                        // delete
+                        nameListData.remove(position);
+                        adapter.notifyDataSetChanged();
+                        break;
+                    case 1:
+                        // open
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
+
+        listView.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT);
+
 
         tv_contact_book_add.setOnClickListener(this);
         iv_contact_book_box.setOnClickListener(this);
@@ -84,6 +167,10 @@ public class ContactBookFragement extends Fragment implements OnClickListener {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
     }
 
     /**得到手机通讯录联系人信息**/
--- app/src/main/java/com/carryj/root/contactbook/fragments/DialFragement.java
+++ app/src/main/java/com/carryj/root/contactbook/fragments/DialFragement.java
@@ -1,8 +1,13 @@
 package com.carryj.root.contactbook.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,6 +15,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.carryj.root.contactbook.MainActivity;
 import com.carryj.root.contactbook.R;
 
 
@@ -19,6 +27,8 @@ import com.carryj.root.contactbook.R;
 
 public class DialFragement extends Fragment implements OnClickListener {
 
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
 
     private TextView tv_dial_add;
     private TextView tv_show_dial_number;
@@ -36,6 +46,7 @@ public class DialFragement extends Fragment implements OnClickListener {
     private Button btn_star;
     private Button btn0;
     private Button btn_jin;
+    private Button btn_call;
 
 
     public DialFragement() {
@@ -66,9 +77,7 @@ public class DialFragement extends Fragment implements OnClickListener {
         btn_star = (Button) view.findViewById(R.id.btn_dial_number_xin);
         btn0 = (Button) view.findViewById(R.id.btn_dial_number_0);
         btn_jin = (Button) view.findViewById(R.id.btn_dial_number_jin);
-
-
-
+        btn_call = (Button) view.findViewById(R.id.btn_dial_number_call);
 
 
         tv_dial_add.setOnClickListener(this);
@@ -86,8 +95,7 @@ public class DialFragement extends Fragment implements OnClickListener {
         btn_star.setOnClickListener(this);
         btn0.setOnClickListener(this);
         btn_jin.setOnClickListener(this);
-
-
+        btn_call.setOnClickListener(this);
 
 
     }
@@ -97,8 +105,10 @@ public class DialFragement extends Fragment implements OnClickListener {
     public void onClick(View v) {
         int id = v.getId();
         switch (id) {
+            //删除拨号最后一个数字
             case R.id.iv_dial_delete:
-                tv_show_dial_number.setText("");
+                String str = tv_show_dial_number.getText().toString();
+                tv_show_dial_number.setText(str.substring(0,str.length()-1));
                 break;
             case R.id.btn_dial_number_1:
                 tv_show_dial_number.append("1");
@@ -136,12 +146,58 @@ public class DialFragement extends Fragment implements OnClickListener {
             case R.id.btn_dial_number_jin:
                 tv_show_dial_number.append("#");
                 break;
+            case R.id.btn_dial_number_call:
+                //调用系统方法拨打电话
+                testCall(v);
+                break;
             default:
                 break;
         }
 
     }
 
+    public void testCall(View view)
+    {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        } else
+        {
+            callPhone();
+        }
+    }
+
+    public void callPhone()
+    {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        Uri data = Uri.parse("tel:" + tv_show_dial_number.getText().toString());
+        intent.setData(data);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                callPhone();
+            } else
+            {
+                // Permission Denied
+                Toast.makeText(getContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
